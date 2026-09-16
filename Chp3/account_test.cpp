ec56083febdde7e66e173aff5ec99d37ee18570f
Muqaddas Ali <muqaddasali681@gmail.com>
2026-09-16 18:51:20 +0500

    Account test case study in OOPS

---
+++ Chp3/account_test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "Account.h"
+using namespace std;
+
+int main() {
+
+    Account myAccount;
+
+    cout << "Initial account name is : " << myAccount.getName();
+    cout << "\nPlease enter the Account name: ";
+    string theName;
+    
+
+
+    return 0;
+    
+}
